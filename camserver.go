9307b1cdfbdac7868b8a817996373f2b1cd1e152
Mike Beller <bellermike@gmail.com>
2017-01-08 12:45:50 -0500

    Get basic framework up without camera itself

--- camserver.go
+++ camserver.go
@@ -4,16 +4,43 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	_ "os"
 	"time"
 )
 
+const tempFilePath = "/tmp/tmp_image.jpg"
+const imageFilePath = "/tmp/image.jpg"
+
+var photoRequests = make(chan int)
+var photoResponses = make(chan string)
+
+func photographer() {
+	for {
+		<-photoRequests
+		time.Sleep(time.Second * 5)
+		photoResponses <- imageFilePath
+	}
+}
+
+func takePic() string {
+	photoRequests <- 1
+	resp := <-photoResponses
+	return resp
+}
+
 func timeHandler(w http.ResponseWriter, r *http.Request) {
-	//time.Sleep(time.Second * 5)
 	ts := time.Now().Format("2006-01-02 15:04:05")
 	fmt.Fprintf(w, "%s\n", ts)
 }
 
+func camHandler(w http.ResponseWriter, r *http.Request) {
+	filePath := takePic()
+	http.ServeFile(w, r, filePath)
+}
+
 func main() {
+	go photographer()
 	http.HandleFunc("/time", timeHandler)
+	http.HandleFunc("/image.jpg", camHandler)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
